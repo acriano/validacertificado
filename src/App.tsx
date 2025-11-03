732e1f452f464781cc31d3d28da17f2cbee3f2ed
ANDERSON ARAUJO DE SOUZA <112498318+acriano@users.noreply.github.com>
2025-11-03 10:07:14 -0500

    att pg 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ValidaCertificado from "./pages/ValidaCertificado";
 import ValidaCertificado2 from "./pages/ValidaCertificado2";
+import ValidaCertificado3 from "./pages/ValidaCertificado3";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/valida-certificado" element={<ValidaCertificado />} />
           <Route path="/valida-certificado-2" element={<ValidaCertificado2 />} />
+          <Route path="/valida-certificado-3" element={<ValidaCertificado3 />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
